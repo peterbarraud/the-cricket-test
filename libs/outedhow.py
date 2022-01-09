a497d41145aad99a0b79e5f19b42e36de8866a58
Peter Barraud <barraud@adobe.com>
2022-01-09 16:36:26 +0530

    handling the outed methods

--- libs/outedhow.py
+++ libs/outedhow.py
@@ -1,7 +1,40 @@
 from enum import Enum
-
+from re import match as rematch, IGNORECASE
 class OutedHow(Enum):
     BOWLED = 1
     CAUGHT = 2
     LBW  = 3
     RUNOUT = 4
+    NOTOUT = 5
+    STUMPED = 6
+    RETIREDOUT = 7
+    HITWICKET = 8
+    RETIREDHURT = 9
+    HANDLEDTHEBALL = 10
+
+def get_outed_how(outedhowstr : str) -> OutedHow:
+    if rematch(pattern=r'\s*c\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.CAUGHT
+    elif rematch(pattern=r'\s*b\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.BOWLED
+    elif rematch(pattern=r'\s*lbw\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.LBW
+    elif rematch(pattern=r'\s*lbw\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.LBW
+    elif rematch(pattern=r'\s*run out\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.RUNOUT
+    elif rematch(pattern=r'\s*not out\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.NOTOUT
+    elif rematch(pattern=r'\s*st\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.STUMPED
+    elif rematch(pattern=r'\s*retired out\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.RETIREDOUT      # TODO: What is "retired out"
+    elif rematch(pattern=r'\s*hit wicket\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.HITWICKET
+    elif rematch(pattern=r'\s*retired hurt\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.RETIREDHURT
+    elif rematch(pattern=r'\s*handled the ball\s+', string=outedhowstr, flags=IGNORECASE):
+        return OutedHow.HANDLEDTHEBALL
+        
+    else:
+        raise Exception(f'Unknowed outed method: {outedhowstr}')
--- make-csv.py
+++ make-csv.py
@@ -4,14 +4,19 @@
 from bs4 import BeautifulSoup as Bs
 from zipfile import ZipFile
 from bs4.element import ResultSet, Tag
+from requests.models import ContentDecodingError
 from libs.config.info import CricketInfo
 from os import walk as oswalk
 from glob import glob
 from os.path import exists
-from re import match as rematch, search as research
+from re import match as rematch, search as research, IGNORECASE
 from requests import get
 from libs.ground import Ground
-from pickle import load, dump
+from pickle import NONE, load, dump
+from libs.outedhow import OutedHow, get_outed_how
+from winsound import Beep
+from timeit import timeit
+from libs.savematchplayedlist import SaveMatchPlayedList
 
 def match_file_generator():
     for (root,dirs,files) in oswalk(top='data.files', topdown=True):
@@ -23,6 +28,11 @@ def match_file_generator():
 def __get_match_soup(match_file):
     unzipped = ZipFile(file=match_file,mode='r')
     return Bs(markup=unzipped.read('matcharchive'), features='html.parser')
+
+def __save_match_file_for_checking(match_file : str):
+    unzipped = ZipFile(file=match_file,mode='r')
+    with open('testing.files/matcharchive.html', 'wb') as f:
+        f.write(unzipped.read('matcharchive'))
 
 def makebowlercsv(info : CricketInfo):
     pass
@@ -49,17 +59,37 @@ def __get_match_details(match_details_card : Tag, grounds) -> tuple:
         if row_count == 0:
             ground : Ground = __get_ground_name_and_country(row, grounds)
         if row.td.get_text() == 'Match number':
-            test_number = research(pattern=r'(\d+?)\s*$',string=row.find_all('td')[1].get_text()).group(1)
+            if research(pattern=r'(\d+?)\s*$',string=row.find_all('td')[1].get_text()) == None:
+                # a freak case of the match being cancelled due to Covid
+                # I believe these matches were not counted. But we'll see
+                print(row.find_all('td')[1].get_text())
+                test_number = research(pattern=r'(\d+?[a-z])\s*$',string=row.find_all('td')[1].get_text()).group(1)
+            else:
+                test_number = research(pattern=r'(\d+?)\s*$',string=row.find_all('td')[1].get_text()).group(1)
     return (test_number, ground.name, ground.country)
 
 
-def makebattercsv(info : CricketInfo):
+def makebattercsv():
+    info : CricketInfo = CricketInfo()
+    smpl = SaveMatchPlayedList()
     grounds = dict()
     if exists("data.files/grounds.dict"):
         with open ("data.files/grounds.dict", 'rb') as f:
             grounds = load(f)
     for match_file in match_file_generator():
+        if smpl.has(match_file):
+            continue
+        if not match_file.endswith('south-africa-vs-england-4th-test-63863.zip'):
+            continue
+        print(match_file)
         soup : Bs = __get_match_soup(match_file=match_file)
+        match_status = soup.find(class_='status-text')
+        # if no "status" available for a match, implies it wasn't play
+        if match_status:
+            if rematch('Match (abandoned|cancelled) without a ball bowled', match_status.get_text()):
+                continue
+        else:   # not status-text was found so don't collect data
+            continue
         # each score has innings cards for the innings played and the last one for the match summary
         match_cards : ResultSet = soup.find_all(class_=info.Scorecard)
         match_summary_card : Tag = match_cards.pop(-1)
@@ -70,35 +100,45 @@ def makebattercsv(info : CricketInfo):
         for innings_number, innings_card in enumerate(match_cards):
             m = rematch(pattern=r'^(.+?)\s+\d', string=innings_card.find('h5').get_text())
             batting_side : str = m.group(1)
-            batter_title_cell : Tag = innings_card.find(class_=info.BatterTitleCell)
-            batter_row : Tag = batter_title_cell.parent()
-            name = batter_title_cell.get_text()
-            print(batter_name_row.parent())
-            break
+            batter_row : Tag = innings_card.find(class_=info.BatterTitleCell).parent
+            (name, outedstr, runs, balls, mins, fours, sixes, _) = (x.get_text() for x in batter_row.find_all('td'))
+            outed_how : OutedHow = get_outed_how(outedstr)
+            smpl.append(match_file)
+            # break
             # print(batting_side, innings_number,test_number, ground, played_in_country)
 
 
-        break
+        # break
 
     with open ("data.files/grounds.dict", 'wb') as f:
          dump(grounds, f)
 
-
-        # if len(batter_score_tables) <= 4:
-        #     for batter_score_table in batter_score_tables:
-        #         for row in batter_score_table.tbody.find_all('tr'):
-        #             if len(row.find_all('td')) == 8:
-        #                 (name_cell, out_cell, runs_cell, balls_cells, mins_cells, fours_cell, sixes_cell, sr_cell) = row.find_all('td')
-        #                 print(name_cell.get_text(), runs_cell.get_text())
-        # else:
-        #     raise Exception(f"Why are there so many batter tables - {len(batter_score_tables)}")
-
-
 # For testing purposes only
 def main():
+    # __save_match_file_for_checking(r'data.files\aus-tour-of-sa-2017-18-1075977\south-africa-vs-australia-4th-test-1075985.zip')
     from libs.config.info import CricketInfo
     makebattercsv(CricketInfo())
+    try:
+        makebattercsv(CricketInfo())
+    except Exception as e:
+        print(e)
+
+def doneit(time_taken_in_sec : float) -> None:
+    time_to_run : float = 0.0
+    unit_of_time = None
+    for _ in range(2):
+        Beep(2000,100)
+    if time_taken_in_sec/60 > 1:
+        time_to_run = round(time_taken_in_sec/60, ndigits=2)
+        unit_of_time = 'mins'
+    else:
+        time_to_run = round(time_taken_in_sec, 2)
+        unit_of_time = 'secs'
+
+    print(f'DONE in : {time_to_run} {unit_of_time}')
 
 if __name__ == '__main__':
-    main()
-    print('DONE')
+    # __save_match_file_for_checking('data.files\england-tour-of-south-africa-1999-00-61687\south-africa-vs-england-5th-test-63864.zip')
+    t = timeit(stmt=makebattercsv, number=1)
+    # test_match_files()
+    # doneit(t)
