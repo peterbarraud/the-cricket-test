35692d859c05440e3581edff6f6a502cfa5cc8aa
Peter Barraud <gapeterb@gmail.com>
2026-01-12 16:29:31 +0530

    fixed headers

--- libs/csvmaker.py
+++ libs/csvmaker.py
@@ -4,26 +4,21 @@ from libs.dataclasses import TeamInfo,PlayerInfo
 class BattingCSV:
     def __init__(self,csv_file='data/batting.csv'):
         self.__f = open(csv_file,'w',newline='')
-        field_names = ['gameid,innings,team,batter,position,runs,balls,fours,sixes,out,fielders,bowler']
+        field_names = ['gameid','innings','team','batter','position','runs',
+                       'balls','fours','sixes','out','fielders','bowler']
+        
         self.__csv_writer = DictWriter(self.__f,field_names)
+        self.__csv_writer.writeheader()
 
     def WriterRow(self, game_id : int, innings_number : int, battingTeamData : TeamInfo):
         player : PlayerInfo = None
         for player in battingTeamData.Team:
-            row : dict = dict()
-            row['gameid'] = game_id
-            row['innings'] = innings_number
-            row['team'] = battingTeamData.Id
-            row['batter'] = player.Id
-            row['runs'] = player.Runs
-            row['balls'] = player.Balls
-            row['fours'] = player.Fours
-            row['sixes'] = player.Sixes
-            row['out'] = player.Out
-            row['fielders'] = "|".join([str(x) for x in player.Fielders])
-            row['bowler'] = player.Bowler
-            row['position'] = player.BattingPosition
-            self.__csv_writer.writerow(row)
+            print("|".join([str(x) for x in player.Fielders]))
+            self.__csv_writer.writerow({'gameid':game_id,'innings':innings_number,'team':battingTeamData.Id,'batter':player.Id,
+                'runs':player.Runs,'balls':player.Balls,'fours':player.Fours,'sixes':player.Sixes,
+                'out':player.Out,'fielders':"|".join([str(x) for x in player.Fielders]),
+                'bowler':player.Bowler,'position':player.BattingPosition
+            })
 
     
 
