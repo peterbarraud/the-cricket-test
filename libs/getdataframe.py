a2cd8fde92ba586406a1aa03ddf1ade97740053e
Peter Barraud <gapeterb@gmail.com>
2026-01-30 10:11:14 +0530

    fixed issue of not sending usecols

--- libs/getdataframe.py
+++ libs/getdataframe.py
@@ -2,10 +2,14 @@ from libs.csvnames import CSVName
 from libs.optimizer import optimize_df
 from pandas import read_csv, DataFrame
 
-def get_dataframe_by_name(csvName : CSVName, optimizeDf : bool = True,  usecols=list()) -> DataFrame:
-    df : DataFrame = read_csv(f'data/{csvName.name.lower()}.csv',usecols=usecols)
+def get_dataframe_by_name(csvName : CSVName, optimizeDf : bool = True,  usecols=list(),showOutput=True) -> DataFrame:
+    df : DataFrame = None
+    if len(usecols):
+        df : DataFrame = read_csv(f'data/{csvName.name.lower()}.csv',usecols=usecols)
+    else:
+        df : DataFrame = read_csv(f'data/{csvName.name.lower()}.csv')
     if optimizeDf:
-        optimize_df(df,do_optimize=True)
+        optimize_df(df,do_optimize=True,displayOutput=showOutput)
     return df
 
 
