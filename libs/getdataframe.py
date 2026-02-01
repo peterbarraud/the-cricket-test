eb678c179117d81c1479f3e3cf1be78840f7da19
Peter Barraud <gapeterb@gmail.com>
2026-02-01 10:04:30 +0530

    a little code opt, and fixing issue of usecols

--- libs/getdataframe.py
+++ libs/getdataframe.py
@@ -4,10 +4,11 @@ from pandas import read_csv, DataFrame
 
 def get_dataframe_by_name(csvName : CSVName, optimizeDf : bool = True,  usecols=list(),showOutput=True) -> DataFrame:
     df : DataFrame = None
+    csv_path : str = f'data/{csvName.name.lower()}.csv'
     if len(usecols):
-        df : DataFrame = read_csv(f'data/{csvName.name.lower()}.csv',usecols=usecols)
+        df : DataFrame = read_csv(csv_path,usecols=usecols)
     else:
-        df : DataFrame = read_csv(f'data/{csvName.name.lower()}.csv')
+        df : DataFrame = read_csv(csv_path)
     if optimizeDf:
         optimize_df(df,do_optimize=True,displayOutput=showOutput)
     return df
