027533634fcb09d135c0c422b6e6802d93188e29
Peter Barraud <gapeterb@gmail.com>
2026-02-01 10:06:47 +0530

    some code opt. but more importantly, now we're checking for if min values of a field is less than zero, then optimization will be for non-uints

--- libs/optimizer.py
+++ libs/optimizer.py
@@ -1,37 +1,36 @@
 from pandas import DataFrame
 from pandas.api.types import is_numeric_dtype
-from numpy import iinfo,uint8,uint16,uint32
+from numpy import iinfo,uint8,uint16,uint32,int8,int16,int32
 
 def optimize_df(df : DataFrame, do_optimize = False,displayOutput=True):
     memory_usage = df.memory_usage(deep=True).sum()
     for col in df.columns:
         if is_numeric_dtype(df[col]):
-            if max(df[col]) < iinfo(uint8).max:
-                if do_optimize:
-                    df[col] = df[col].astype(uint8)
-                    if displayOutput:
-                        print(f"{col} is optimized to: {uint8}")
-                else:
-                    print(f"{col} can be optimized to: {uint8}")
+            astype = None
+            if min(df[col]) < 0:
+                if max(df[col]) <= iinfo(int8).max:
+                    astype = int8
+                elif max(df[col]) <= iinfo(int16).max:
+                    astype = int16
+                elif max(df[col]) <= iinfo(int32).max:
+                    astype = int32
             else:
-                if iinfo(uint8).max < max(df[col]) < iinfo(uint16).max:
-                    if do_optimize:
-                        df[col] = df[col].astype(uint16)
-                        if displayOutput:
-                            print(f"{col} is optimized to: {uint16}")
-                    else:
-                        print(f"{col} can be optimized to: {uint16}")
+                if max(df[col]) <= iinfo(uint8).max:
+                    astype = uint8
+                elif max(df[col]) <= iinfo(uint16).max:
+                    astype = uint16
+                elif max(df[col]) <= iinfo(uint32).max:
+                    astype = uint32
+            if astype is not None:
+                if do_optimize:
+                    df[col] = df[col].astype(astype)
+                    if displayOutput:
+                        print(f"{col} is optimized to {astype}")
                 else:
-                    if iinfo(uint16).max < max(df[col]) < iinfo(uint32).max:
-                        if do_optimize:
-                            if displayOutput:
-                                print(f"{col} is optimized to: {uint32}")
-                            df[col] = df[col].astype(uint32)
-                        else:
-                            print(f"{col} can be optimized to: {uint16}")
-                    else:
-                        if displayOutput:
-                            print(f"{col} cannot be optimized further")
+                    print(f"{col} can be optimized to {astype}")
+            else:
+                if displayOutput:
+                    print(f"{col} cannot be optimized further")
     memory_reduction = df.memory_usage(deep=True).sum()-memory_usage
     improvement = round((memory_reduction/memory_usage)/100,2)
     print(f"Memory improved by: {(improvement)}")
